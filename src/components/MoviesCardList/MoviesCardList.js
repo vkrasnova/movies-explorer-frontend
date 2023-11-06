31e6ed550b2593416022e68a34bbc1f14d9ae7ce
Valentina Krasnova <github@krasnova.info>
2023-11-06 19:11:57 +0300

    feat: 'MoviesCardList' (in progress)

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,27 +1,33 @@
 import MoviesCard from '../MoviesCard/MoviesCard';
 import sampleMovies from './sampleMovies';
+import useResize from '../../hooks/useResize';
 
 import ('./MoviesCardList.css');
 
 const MoviesCardList = () => {
+
+  const { isScreenM, isScreenL } = useResize();
 
   return (
     <section className="movies">
       <div className="movies__container">
         <ul className="movies__list">
 
-            {sampleMovies.map((item) => {
-              return (
-                <li>
-                  <MoviesCard
-                  key={item._id}
-                  movie={item}
-                  // onCardLike={onCardLike}
-                  // onCardDelete={onCardDelete}
-                  />
-                </li>
-              )
-            })}
+            {sampleMovies
+              .slice(0, `${isScreenL ? 16 : isScreenM ? 8 : 5}`)
+              .map((item) => {
+                return (
+                  <li>
+                    <MoviesCard
+                    key={item._id}
+                    movie={item}
+                    // onMovieSave={onMovieSave}
+                    // onMovieDelete={onMovieDelete}
+                    />
+                  </li>
+                )
+              })
+            }
 
           </ul>
 
