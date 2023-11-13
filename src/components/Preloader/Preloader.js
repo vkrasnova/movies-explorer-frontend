7534c2f84a4a6e2b03bf92e2a01ded4236719f64
Valentina Krasnova <github@krasnova.info>
2023-11-13 03:58:48 +0300

    refactor: 'Preloader' (edits in CSS)

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Preloader.css';
 
-const Preloader = () => {
+const Preloader = ({ fullScreen }) => {
   return (
-    <div className="preloader">
+    <div className={`preloader ${fullScreen ? 'preloader_fullscreen' : ''}`}>
       <div className="preloader__container">
         <span className="preloader__round"></span>
       </div>
