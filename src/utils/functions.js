cba5815bb339dc6b0f8b16c9c925c086c557c082
Valentina Krasnova <github@krasnova.info>
2023-11-15 15:36:36 +0300

    feat: add function 'unifyMoviesData'

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,22 @@
+import { URL } from '../utils/constants';
+
+export const unifyMoviesData = (movies) => {
+  return movies.map((item) => {
+    return {
+      movieID: item.id,
+      nameRU: item.nameRU,
+      nameEN: item.nameEN,
+      description: item.description,
+      thumbnail: `${URL.MOVIES}${item.image.formats.thumbnail.url}`,
+      image: `${URL.MOVIES}${item.image.url}`,
+      trailerLink: item.trailerLink,
+      year: item.year,
+      country: item.country,
+      director: item.director,
+      duration: item.duration,
+    }});
+}
+
 export const convertDurationToText = (duration) => {
   const hours = Math.floor(duration/60);
   const minutes = duration % 60;
